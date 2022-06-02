feed714ecd0fa4aec5d76066ac9403e7c4163907
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-06-02 21:45:37 +0530

    Add MainActivity

--- app/src/main/java/com/iam2kabhishek/trafficlight/MainActivity.java
+++ app/src/main/java/com/iam2kabhishek/trafficlight/MainActivity.java
@@ -1,14 +1,23 @@
 package com.iam2kabhishek.trafficlight;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+//MainActivity listens to ButtonClick Listeners from the Fragment
+public class MainActivity extends AppCompatActivity implements Listener{
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked() {
+        TrafficLightFragment fragment = (TrafficLightFragment) getSupportFragmentManager().findFragmentById(R.id.light_fragment);
+        //when the button is clicked in the ButtonFragment
+        fragment.onButtonClicked();
+    }
+
+    @Override
+    public void clickButton() {
+        onButtonClicked();
     }
 }
