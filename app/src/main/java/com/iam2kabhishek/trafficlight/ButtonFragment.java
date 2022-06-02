2d29fbe98e738c4080c910050c66ce07fd150ef5
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-06-02 21:46:24 +0530

    Add ButtonFragment

---
+++ app/src/main/java/com/iam2kabhishek/trafficlight/ButtonFragment.java
@@ -0,0 +1,40 @@
+package com.iam2kabhishek.trafficlight;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ButtonFragment extends Fragment {
+    private Listener listener;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_button, container, false);
+        Button button = view.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (listener != null) {
+                    listener.clickButton();
+                }
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof Listener) {
+            listener = (Listener) context;
+        }
+    }
+}
